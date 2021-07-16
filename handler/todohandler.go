ef70db7165266418fdfb55d4fd2beb481c3e3bbe
Junior Pereira <pereirajr@Junin-Mecbook.local>
2021-07-16 15:54:34 -0300

    change health response to 2.0

--- handler/todohandler.go
+++ handler/todohandler.go
@@ -11,7 +11,7 @@ import (
 )
 // Health returns that server is Up
 func Health(responseWriter http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(responseWriter, "Server OK 1.0")
+	fmt.Fprint(responseWriter, "Server OK 2.0")
 }
 
 func GetTodo(responseWriter http.ResponseWriter, request *http.Request) {
